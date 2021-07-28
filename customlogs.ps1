c143e5874a0fa3b93860beefdf0227f1b3b4670f
DavidAamcomp <87546072+DavidAamcomp@users.noreply.github.com>
2021-07-28 17:25:20 -0500

    Update customlogs.ps1

--- customlogs.ps1
+++ customlogs.ps1
@@ -79,3 +79,53 @@ $json = $json | ConvertTo-Csv | ConvertFrom-Csv | ConvertTo-Json
 Send-OMSAPIIngestionFile -customerId $customerId -sharedKey $SharedKey -body $json -logType $LogType
 Set-Location $dir
  }
+ 
+$LogType = "SpeedTest" 
+$DownloadURL = "https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-win64.zip"
+#location to save on the computer. Path must exist or it will error
+$DOwnloadPath = "c:\temp\SpeedTest.Zip"
+$ExtractToPath = "c:\temp\SpeedTest"
+$SpeedTestEXEPath = "C:\temp\SpeedTest\speedtest.exe"
+#Log File Path
+$LogPath = 'c:\temp\SpeedTestLog.txt'
+
+#Start Logging to a Text File
+$ErrorActionPreference="SilentlyContinue"
+#Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+#Start-Transcript -path $LogPath -Append:$false
+#check for and delete existing log files
+
+function RunTest()
+{
+    $test = & $SpeedTestEXEPath --accept-license --format=json
+    $test
+}
+
+if (Test-Path $SpeedTestEXEPath -PathType leaf)
+{
+    #Write-Host "SpeedTest EXE Exists, starting test" -ForegroundColor Green
+    $json = RunTest
+}
+else
+{
+    #Write-Host "SpeedTest EXE Doesn't Exist, starting file download"
+
+    #downloads the file from the URL
+    wget $DownloadURL -outfile $DOwnloadPath
+
+    #Unzip the file
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    function Unzip
+    {
+        param([string]$zipfile, [string]$outpath)
+
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+    }
+
+    Unzip $DOwnloadPath $ExtractToPath
+    RunTest
+}
+
+$json | Add-Member -MemberType NoteProperty -Name "Computer" -Value $hostname
+Send-OMSAPIIngestionFile -customerId $customerId -sharedKey $SharedKey -body $json -logType $LogType
